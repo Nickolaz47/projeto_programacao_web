ac2582d284ead57338c2cd08829e55d8ccd92e85
JaoVit0r <joaovitor.nunes.ismart@gmail.com>
2022-06-19 17:38:23 -0300

    Added query params

--- server/games/router.js
+++ server/games/router.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const gamesRouter = express.Router();
-const app = express();
+const gamesRouter = express.Router(); // será utilizado router depois
+const app = express(); // por hora, utilizado para testar com postman
 const fsPromises = require('fs/promises');
 // const path = require('path');
 
@@ -16,12 +16,25 @@ const readDb = async () => {
     return jsonParsed
 };
 
-console.log(cutDir);
-
 app.get('/games', async (req,res) => {
+    console.clear();
     const games = await readDb();
-    const { limit } = req.query;
+    const { limit, sortBy, name } = req.query;
     const listGamesLimited = games.data.slice(0,limit);
+    const nomesGames = games.data.map(game => game.name);
+    const nomesGamesSorted = nomesGames.sort();
+    const gamesSorted = [];
+
+    for (index in games.data) {
+        gamesSorted[index] = games.data.find(game => game.name === nomesGamesSorted[index]);
+    }
+
+    if(sortBy) {
+        if(sortBy === 'name') {
+            return res.json(gamesSorted)
+        }
+        res.status(400).send('Critério de organização não definido') // É esse mesmo o status code?
+    }
 
     if(limit) {
         if(limit > games.data.length) {
@@ -29,8 +42,17 @@ app.get('/games', async (req,res) => {
         }
         return res.json(listGamesLimited)
     }
+
+    if(name) {
+        const gameFilteredByName = games.data.filter(game => game.name === name);
+        res.json(gameFilteredByName);
+    }
     res.json(games.data);
 });
+
+
+
+// console.log([11,2,22,1].sort()) // ver lógica do sort 
 
 app.get('/games/:gameID', async (req,res) => {
     const games = await readDb();

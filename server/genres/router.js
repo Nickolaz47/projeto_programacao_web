23d8803794ea60f43708ef917bf3ae6463289cfc
Rodrigo Mendes <rodrigom.souza1999@gmail.com>
2022-06-20 20:06:57 -0300

    small changes

---
+++ server/genres/router.js
@@ -0,0 +1,80 @@
+const express = require('express')
+const fs = require('fs/promises')
+const genresRouter = express.Router()
+const db = './server/db.json'
+
+const readDb = async () => {
+    const dbJSON = await fs.readFile(db)    
+    const jsonParsed = JSON.parse(dbJSON)
+    return jsonParsed
+}
+
+const generateId = async () => {
+    const dbJSON = await readDb()    
+    if (dbJSON.length > 0) {
+        const idList = dbJSON.map((obj) => obj.id).sort()        
+        const lastId = Number(idList.slice(-1))        
+        return lastId + 1
+    } else {
+        return 1
+    }        
+}
+
+genresRouter.get('/', async (req, res) => {
+    const dbJSON = await readDb()    
+    res.json(dbJSON);
+})
+
+genresRouter.post('/', async (req, res) => {
+    const companiesDb = await readDb()        
+    const newRegister = req.body    
+    newRegister.id = await generateId()    
+    companiesDb.push(newRegister)
+    await fs.writeFile(db, JSON.stringify(companiesDb))
+    res.status(201).json(newRegister)
+})
+
+genresRouter.put('/:companyId', async (req, res) => {
+    const companiesDb = await readDb()
+    const {name, foundation, headquarters, logo, games} = req.body
+    const {companyId} = req.params
+    const companyIdNumber = Number(companyId)
+    const companyToUpdate = companiesDb.find(
+        company => company.id === companyIdNumber
+    )
+    
+    if(companyToUpdate !== undefined) {        
+        companiesDb.forEach(company => {
+            if (company.id === companyIdNumber) {
+                company.name = name === undefined ? companyToUpdate.name : name
+                company.foundation = foundation === undefined ? companyToUpdate.foundation : foundation
+                company.headquarters = headquarters === undefined ? companyToUpdate.headquarters : headquarters
+                company.logo = logo === undefined ? companyToUpdate.logo : logo
+                company.games = games === undefined ? companyToUpdate.games : games
+            }
+        })
+        await fs.writeFile(db, JSON.stringify(companiesDb))
+        res.status(200).send(`Atualizado!`)
+    } else {
+        res.status(400).send('Id inválido!')
+    }
+})
+
+genresRouter.delete('/:companyId', async (req, res) => {
+    const companiesDb = await readDb()
+    const {companyId} = req.params
+    const companyIdNumber = Number(companyId)    
+    const companyToDelete = companiesDb.find(
+        company => company.id === companyIdNumber
+    )
+
+    if(companyToDelete !== undefined) {       
+        companiesDb.splice(companiesDb.indexOf(companyToDelete), 1)
+        await fs.writeFile(db, JSON.stringify(companiesDb))        
+        res.status(200).send(companyToDelete)
+    } else {
+        res.status(400).send('Id inválido!')
+    }
+})
+
+module.exports = genresRouter
--- server/server.js
+++ server/server.js
@@ -2,10 +2,14 @@
 const express = require('express')
 const app = express()
 const companiesRouter = require('./companies/router')
+const genresRouter = require('./genres/router')
+const gamesRouter = require('./games/router')
 
 app.use(express.json())
 
 app.use('/companies', companiesRouter)
+app.use('/games', gamesRouter)
+app.use('/genres', genresRouter)
 
 app.use('*', (req, res) => {
   res.send('Não há mapeamento para essa rota!')

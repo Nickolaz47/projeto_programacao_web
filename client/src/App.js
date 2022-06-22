5e50608f7e953ffba8375f05a12132b1e85f8258
Nicolas da Matta <nicolasaraujo47@gmail.com>
2022-06-22 20:06:05 -0300

    Finished React app main structure.

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Game from "./pages/Game";
+import axios from "axios";
 
 function App() {
+  const url = "";
+  const games = axios.get(url);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home games={games} />}></Route>
+          <Route path="/game/:id" element={<Game />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
